537a1e5ad780aaf22ac7afbf644a14765c5066a8
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2024-10-31 10:59:39 +0100

    use test images

--- zeroshot.sh
+++ zeroshot.sh
@@ -6,8 +6,8 @@
 DATASET=$1
 
 python saliency_maps/generate_saliency_maps.py \
---input-path ${DATASET}/images \
---output-path saliency_map_outputs/${DATASET}/masks \
+--input-path ${DATASET}/test_images \
+--output-path saliency_map_outputs/${DATASET}/test_masks \
 --val-path ${DATASET}/val_images \
 --model-name BiomedCLIP \
 --finetuned \
@@ -15,22 +15,22 @@ python saliency_maps/generate_saliency_maps.py \
 --val-path ${DATASET}/val_images
 
 python postprocessing/postprocess_saliency_maps.py \
---input-path ${DATASET}/images \
---output-path coarse_outputs/${DATASET}/masks \
---sal-path saliency_map_outputs/${DATASET}/masks \
+--input-path ${DATASET}/test_images \
+--output-path coarse_outputs/${DATASET}/test_masks \
+--sal-path saliency_map_outputs/${DATASET}/test_masks \
 --postprocess kmeans \
 # --num-contours 2 # number of contours to extract, for lungs, use 2 contours
 
 python segment-anything/prompt_sam.py \
---input ${DATASET}/images \
---mask-input coarse_outputs/${DATASET}/masks \
---output sam_outputs/${DATASET}/masks \
+--input ${DATASET}/test_images \
+--mask-input coarse_outputs/${DATASET}/test_masks \
+--output sam_outputs/${DATASET}/test_masks \
 --model-type vit_h \
 --checkpoint segment-anything/sam_checkpoints/sam_vit_h_4b8939.pth \
 --prompts boxes \
 # --multicontour # for lungs, use this flag
 
 python evaluation/eval.py \
---gt_path ${DATASET}/masks \
---seg_path sam_outputs/${DATASET}/masks \
+--gt_path ${DATASET}/test_masks \
+--seg_path sam_outputs/${DATASET}/test_masks \
 --save_path sam_outputs/${DATASET}/test.csv 
